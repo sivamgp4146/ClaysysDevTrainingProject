ea487164820ecf5d55ea6763827bed8e7a9644da
ayyavu.sivakumar <ayyavu.sivakumar@SYSLP1220>
2023-06-15 16:27:18 +0530

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ClaysysDevTrainingProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClaysysDevTrainingProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// This action is used to show the login page view with internal style sheet.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LoginForm()
+        {
+            return View();
+        }
+        /// <summary>
+        /// This action is used to show the login page view with external style sheet.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LoginFormExtCSS()
+        {
+            return View();
+        }
+        /// <summary>
+        /// This action is used to show the Student registration page view with internal style sheet.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult StudentRegistration()
+        {
+            return View();
+        }
+        /// <summary>
+        /// This action is used to show the Student registration page view with external style sheet.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult StudentRegistrationExtCSS()
+        {
+            return View();
+        }
+        /// <summary>
+        /// This action is used to show the Student mark entry page view with external style sheet.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult StudentMarkEntry()
+        {
+            return View();
+        }
+    }
+}
